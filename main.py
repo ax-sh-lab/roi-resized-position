a088362b9ba7d56d4a57b98e99453c8598a3937f
Axm <56112475+ax-sh@users.noreply.github.com>
2021-10-03 20:19:04 +0545

    remove deps

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import cv2  
 from pathlib import Path 
-from roi import RatioRoi 
-import imutils
+from roi import RatioRoi
 
 path = Path(__file__).parent
 
 def circle(image, position, color=(255,0,0)):
     cv2.circle(image, position, 1, color, 5 )
 
+def resize(image,window_height = 500):
+    height, width = image.shape[:2]
+
+    aspect_ratio = float(height/width)
+    window_width = window_height/aspect_ratio
+    image = cv2.resize(image, (int(window_height),int(window_width)))
+    return image
+
+
 img_path = path / 'image.webp'
 img = cv2.imread(str(img_path))
-resized = imutils.resize(image=img, width=200)
+resized = resize(image=img, window_height=200)
 
 ratio = RatioRoi(img, resized)
 pos = (40,130)
